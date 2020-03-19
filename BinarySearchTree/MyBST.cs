d3decb862317900940c0840e480d23170084943c
Alan.Lin <eric820209@gmail.com>
2020-03-19 23:41:03 +0800

    基本架構

---
+++ BinarySearchTree/MyBST.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    public class MyBST<T> where T :class
+    {
+        public Node<T> Root { get; set; }
+
+        public void Add(int Index,T NewValue)
+        { 
+        
+        }
+        public void DeleteAt(int Index)
+        {
+        
+        }
+        public T Search(int Index)
+        { 
+        
+        }
+
+
+    }
+}
---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    public class Node<T>  where T : class
+    {
+        public int Index { get; set; }
+        public T Value { get; set; }
+        public Node<T> LeftNode { get; set; }
+        public Node<T> RightNoed { get; set; }
+    }
+}
